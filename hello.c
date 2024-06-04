c710eaa5fe6952da9c0b31868b67699491f7883f
G M Tasnim Alam <shovon0203@yahoo.com>
2024-06-04 13:46:14 -0400

    quine program

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char *s = "#include <stdio.h>%c%cint main() {%c%cchar *s = %c%s%c;%c%c%cprintf(s,10,10,9,34,s,34,10,10,9,10,9,10);%c%creturn 0;%c}";
+
+    printf(s,10,10,10,9,34,s,34,10,10,9,10,9,10);
+    return 0;
+}
