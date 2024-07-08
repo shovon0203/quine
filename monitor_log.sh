ab95fcc66063435882ad5934da9ed59d1b1e989c
shovon0203 <shovon0203@yahoo.com>
2024-07-08 00:20:14 -0400

    Create monitor_log.sh

---
+++ monitor_log.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LOG_FILE="/var/log/osquery/osqueryd.results.log"
+
+if [ -s "$LOG_FILE" ]; then
+    echo "Suspicious activity detected!"
+    return 1
+  else
+    echo "No suspicious activity detected."
+    return 0
+  fi
